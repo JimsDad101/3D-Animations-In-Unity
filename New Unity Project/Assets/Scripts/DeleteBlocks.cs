dbe818e5a0bb782914af700dfd35331383269a31
JimsDad101 <harrywarland12345@gmail.com>
2020-11-18 09:38:03 +0000

    New Game Mechanic Off Deleting Blocks
    asd

---
+++ New Unity Project/Assets/Scripts/DeleteBlocks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBlocks : MonoBehaviour
+{
+
+    public GameObject block;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Sphere")
+        {
+            Debug.Log("Collision On White Block");
+            // Checks Collision Against The Wall
+            Destroy(gameObject);
+            // Destroys The White Block
+        }
+    }
+}
